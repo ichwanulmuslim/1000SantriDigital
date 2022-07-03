8853a638551ca67d2df3896c3dbc567f4a7e1fae
ichwan.devs@gmail.com <ichwan234>
2022-07-03 09:11:43 +0700

    pertemuan 5

--- app/src/main/java/com/daarulhijrah/rawi/WebviewActivity.java
+++ app/src/main/java/com/daarulhijrah/rawi/WebviewActivity.java
@@ -2,6 +2,8 @@ package com.daarulhijrah.rawi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Bundle;
 import android.webkit.JavascriptInterface;
@@ -10,73 +12,49 @@ import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 public class WebviewActivity extends AppCompatActivity {
 
+    WebView mywebview;
+
     @Override
+    @SuppressLint("SetJavaScriptEnabled")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
 
-        WebView mywebview = (WebView) findViewById(R.id.webView);
-//        mywebview.loadUrl("https://rawi.daarulhijrah.com");
+        getSupportActionBar().hide();
+
+        mywebview = (WebView) findViewById(R.id.webView);
         WebSettings webSettings = mywebview.getSettings();
         webSettings.setJavaScriptEnabled(true);
-//        mywebview.setWebViewClient(new MyWebViewClient());
-//        mywebview.setWebChromeClient(new MyWebChromeClient());
-//        mywebview.addJavascriptInterface(new JavaScriptInterface(this), "AndroidFunction");
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
 
+        mywebview.setWebViewClient(new MyWebViewClient());
         mywebview.loadUrl("file:///android_asset/index.html");
     }
 
-    private class MyWebViewClient extends WebViewClient {
-        @Override public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-            return super.shouldOverrideUrlLoading(view, request);
-        }
-
-        @Override public void onPageFinished(WebView view, String url) {
-        }
-
-        @Override
-        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-            super.onReceivedError(view, request, error);
+    @Override
+    public void onBackPressed() {
+        if(mywebview.canGoBack()) {
+            mywebview.goBack();
+        } else {
+            super.onBackPressed();
         }
     }
 
-    private class MyWebChromeClient extends WebChromeClient {
-        @Override
-        public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
-            return true;
-        }
-
-        @Override
-        public boolean onJsConfirm(WebView view, String url, String message, final JsResult result) {
-            return true;
-        }
-
-        @Override
-        public boolean onJsPrompt(WebView view, String url, String message, String defaultValue,
-                                  final JsPromptResult result) {
-            return true;
-        }
-    }
-
-    public class JavaScriptInterface {
-        Context mContext;
-
-        JavaScriptInterface(Context c) {
-            mContext = c;
-        }
-
-        @JavascriptInterface
-        public void showToast(String toast) {
-            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
-        }
-    }
 
 
 }
